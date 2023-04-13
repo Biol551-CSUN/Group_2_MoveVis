2c246fda55ce45b692e44d744ccb791145bbb983
Kenta Shimizu <kenta.shimizu.678@my.csun.edu>
2023-04-13 12:08:57 -0700

    add

--- Kenta/script/myscript20230407.R
+++ Kenta/script/myscript20230407.R
@@ -6,10 +6,44 @@ library(maps)
 library(mapdata)
 library(mapproj)
 library(janitor)
+library(moveVis)
+library(move)
+
+cats<- cats_uk%>%
+  select(location_long, location_lat, timestamp)%>%
+  drop_na()
+ 
+
+view(cats_uk)
+
+data("cats_uk", package= "movieVis" )
+
+m <- align_move(move_data, res = 4, unit = "mins")
+
+# if your tracks are present as data.frames, see df2move() for conversion
+
+# align move_data to a uniform time scale
+m <- align_move(cats_uk, res = 4, unit = "mins")
+
+# create spatial frames with a OpenStreetMap watercolour map
+frames <- frames_spatial(m, path_colours = c("red", "green", "blue"),
+                         map_service = "osm", map_type = "watercolor", alpha = 0.5) %>% 
+  add_labels(x = "Longitude", y = "Latitude") %>% # add some customizations, such as axis labels
+  add_northarrow() %>% 
+  add_scalebar() %>% 
+  add_timestamps(type = "label") %>% 
+  add_progress()
+
+frames[[100]] # preview one of the frames, e.g. the 100th frame
+
+# animate frames
+animate_frames(frames, out_file = "moveVis.gif")
 
 cats_uk <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-01-31/cats_uk.csv')
 cats_uk_reference <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-01-31/cats_uk_reference.csv')
 UK <- map_data("world", region = "UK")
+
+view(cats_uk)
 
 ggplot()+
   geom_polygon(data = UK,
